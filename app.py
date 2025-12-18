64e3e033ddaa67e6925d721fd8f5143119c8d469
Mouhamadou Moustapha Gaye <64489795+tafa1618@users.noreply.github.com>
2025-12-18 16:56:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from datetime import date
-from kpi.productivite import page_productivite
+from kpis.productivite import page_productivite
 
 # ==================================================
 # CONFIG
